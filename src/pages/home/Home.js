cc0d525c1d2a04a9b4991d6a0078acc80e3a4b45
BarryTong98 <barrytong98@gmail.com>
2023-01-29 15:16:21 +0800

    init project & create login/signup page

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import styles from './Home.module.css'
+
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div>
+            Home
+        </div>
+    );
+}
+
+export default Home;
